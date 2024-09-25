3ddf5b45f1a80d9721b5ff6c2ed67c11402dbde5
vilivonka <gromootvod@lenta.ru>
2024-09-25 15:26:19 +0300

    first commit

---
+++ js/init.js
@@ -0,0 +1,10 @@
+$('.wrappers_slider').slick({
+  dots: false,
+  infinite: true,
+  prevArrow: $('.wrappers_slider_nav_prev'),
+  nextArrow: $('.wrappers_slider_nav_next'),
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: false,
+  autoplay: false,
+});
